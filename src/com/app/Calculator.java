31046f27fc92ce6a5e3cb75153d29b73d3c38bb3
Shweth Kenkre <kenkreshweth@gmail.com>
2022-06-21 10:43:55 +0530

    Java calculator added

---
+++ src/com/app/Calculator.java
@@ -0,0 +1,84 @@
+package com.app;
+import java.lang.Math;
+import java.util.Scanner;
+
+public class Calculator {
+private static int X,Y;
+Scanner s= new Scanner(System.in);
+private double R;
+public void init(int X,int Y){
+	this.X=X;
+	this.Y=Y;
+}
+public void add()
+{
+	System.out.println("Add");
+	R=X+Y;
+}
+public void multiply()
+{
+	System.out.println("Multiplication");
+	R=X*Y;
+}
+public void power()
+{
+	int f=1;
+	System.out.println("Power");
+	//R=Math.pow(X,Y);
+	for(int i=1;i<=Y;i++)
+	{
+		f*=X;
+	}
+	R=f;
+}
+
+public void display()
+{
+	//System.out.println("Power");
+	System.out.println("The value is :"+R);
+}
+public static void main(String[] args)
+{
+	char choice;
+	Scanner s= new Scanner(System.in);
+	Calculator m= new Calculator();
+	System.out.println("Enter your choice\nA: Addition\nB: Multiplication\nC: Power\n Any number to Exit");
+	choice=s.next().charAt(0);
+	switch(choice){
+	
+		case 'A':
+		{
+			System.out.println("Enter the 2 numbers");
+			X=s.nextInt();
+			Y=s.nextInt();
+			m.init(X, Y);
+			m.add();
+			m.display();
+			break;
+		}
+		case 'B':
+		{
+			System.out.println("Enter the 2 numbers");
+			X=s.nextInt();
+			Y=s.nextInt();
+			m.init(X, Y);
+			m.multiply();
+			m.display();
+			break;
+		}
+		case 'C':
+		{
+			System.out.println("Enter the 2 numbers");
+			X=s.nextInt();
+			Y=s.nextInt();
+			m.init(X, Y);
+			m.power();
+			m.display();
+			break;
+		}
+		default: 
+			break;
+	}
+}
+}
+
